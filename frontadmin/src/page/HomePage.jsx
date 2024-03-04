b6a8eb3f36fd3df5a86a346e1fcb46620fa21743
mupxn <noey.ppk123@gmail.com>
2024-03-05 03:46:50 +0700

    edit chart

--- frontadmin/src/page/HomePage.jsx
+++ frontadmin/src/page/HomePage.jsx
@@ -5,21 +5,26 @@ import PieChart from "../element/PieChart.jsx";
 import LineChart from "../element/LineChart.jsx";
 
 function HomePage() {
-  
+
   return (
     <div className="Home">
       <div className="head-home">HomePage</div>
-      <div className="bar-chart">
-        <BarChart/>
+      <div className="chart">
+        <div className="s-chart">
+          <div className="bar-chart">
+            <BarChart />
+          </div>
+          <div className="pie-chart">
+            <PieChart />
+          </div>
+        </div>
+        <div className="line-chart">
+          <LineChart />
+        </div>
       </div>
-      <div className="pie-chart">
-        <PieChart/>
-      </div>
-      <div className="line-chart">
-        <LineChart/>
-      </div>
+
     </div>
-    
+
   )
 }
 
